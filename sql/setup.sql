a8224b60a4f5dbb7c6f66c611173eb815a95bc00
Andrea Cleland <andreaclelan@gmail.com>
2022-06-09 11:23:48 -0700

    added dino data into setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists dinos;
+
+CREATE table dinos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  period VARCHAR NOT NULL
+);
+
+INSERT INTO dinos (name, period) VALUES 
+('Iguanodon', 'Cretaceous'),
+('Tyrannosaurus', 'Cretaceous'),
+('Apatosaurus', 'Jurassic'),
+('Dilophosaurus', 'Jurassic'),
+('Ankylosuaurus', 'Cretaceous'),
+('Stegosaurus', 'Jurassic'),
+('Triceratops', 'Cretaceous'),
+('Pteronodon', 'Cretaceous'),
+('Dimetrodon', 'Triassic'),
+('Diplodocus', 'Jurassic'),
+('Parasaurolophus', 'Cretaceous'),
+('Spinosaurus', 'Crecaceous'),
+('Velociraptor', 'Cretaceous'),
+('Spike the Puppy Dragon', 'Quaternary');
