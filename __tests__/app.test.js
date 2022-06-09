37acb1c2ef2accaaf1fa1c22f12519a903f1579e
Andrea Cleland <andreaclelan@gmail.com>
2022-06-09 11:50:14 -0700

    immplemented list Dino model and list route, first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,7 +8,78 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
   it('/dinos should return list of dinosaurs', async () => {
-    const expected = [{ id: '1', name: 'Iguanodon' }];
+    const expected = [
+      {
+        id: '1',
+        name: 'Iguanodon',
+        period: 'Cretaceous',
+      },
+      {
+        id: '2',
+        name: 'Tyrannosaurus',
+        period: 'Cretaceous',
+      },
+      {
+        id: '3',
+        name: 'Apatosaurus',
+        period: 'Jurassic',
+      },
+      {
+        id: '4',
+        name: 'Dilophosaurus',
+        period: 'Jurassic',
+      },
+      {
+        id: '5',
+        name: 'Ankylosuaurus',
+        period: 'Cretaceous',
+      },
+      {
+        id: '6',
+        name: 'Stegosaurus',
+        period: 'Jurassic',
+      },
+      {
+        id: '7',
+        name: 'Triceratops',
+        period: 'Cretaceous',
+      },
+      {
+        id: '8',
+        name: 'Pteronodon',
+        period: 'Cretaceous',
+      },
+      {
+        id: '9',
+        name: 'Dimetrodon',
+        period: 'Triassic',
+      },
+      {
+        id: '10',
+        name: 'Diplodocus',
+        period: 'Jurassic',
+      },
+      {
+        id: '11',
+        name: 'Parasaurolophus',
+        period: 'Cretaceous',
+      },
+      {
+        id: '12',
+        name: 'Spinosaurus',
+        period: 'Crecaceous',
+      },
+      {
+        id: '13',
+        name: 'Velociraptor',
+        period: 'Cretaceous',
+      },
+      {
+        id: '14',
+        name: 'Spike the Puppy Dragon',
+        period: 'Quaternary',
+      },
+    ];
     const res = await request(app).get('/dinos');
     expect(res.body).toEqual(expected);
   });
